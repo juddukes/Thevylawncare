4def53c8b2db8043be5d59f136896a9b5d08ad7c
jmdukes <jmdukes@iu.edu>
2025-07-10 13:19:31 -0400

    Skeleton index page

---
+++ home.php
@@ -0,0 +1,16 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>GreenEdge Landscaping</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Welcome to GreenEdge Landscaping</h1>
+  <p>Professional landscaping and lawn care in your neighborhood.</p>
+  <ul>
+    <li><a href="contact.php">Contact Us</a></li>
+    <li><a href="quote.php">Request a Quote</a></li>
+  </ul>
+</body>
+</html>
